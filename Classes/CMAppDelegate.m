55a546395fdc70b69a3eb88af8cc819e2b47a99b
Leif Harrison <leif@ubermind.com>
2012-11-30 21:42:37 -0800

    In debug mode, if persistent store creation fails, delete any existing store and recreate it.

--- Classes/CMAppDelegate.m
+++ Classes/CMAppDelegate.m
@@ -198,10 +198,8 @@
 	
     NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"Pathfinder.CDBStore"];
 	
-    /*
-     Set up the store.
-     For the sake of illustration, provide a pre-populated default store.
-     */
+    // Set up the store.
+
     NSFileManager *fileManager = [NSFileManager defaultManager];
     // If the expected store doesn't exist, copy the default store.
     if (![fileManager fileExistsAtPath:[storeURL path]]) {
@@ -212,35 +210,24 @@
     }
 	
     NSDictionary *options = @{NSMigratePersistentStoresAutomaticallyOption: @(YES), NSInferMappingModelAutomaticallyOption: @(YES)};
-    persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: [self managedObjectModel]];
+    persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
 	
     NSError *error;
     if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error]) {
-        /*
-         Replace this implementation with code to handle the error appropriately.
-         
-         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-         
-         Typical reasons for an error here include:
-         * The persistent store is not accessible;
-         * The schema for the persistent store is incompatible with current managed object model.
-         Check the error message to determine what the actual problem was.
-         
-         
-         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
-         
-         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
-         * Simply deleting the existing store:
-         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
-         
-         * Performing automatic lightweight migration by passing the following dictionary as the options parameter:
-         @{NSMigratePersistentStoresAutomaticallyOption:@YES, NSInferMappingModelAutomaticallyOption:@YES}
-         
-         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
-         
-         */
+#ifdef DEBUG
+		// Remove the existing file and try again...
+		if ([fileManager fileExistsAtPath:[storeURL path]]) {
+			NSError *error = nil;
+			[fileManager removeItemAtPath:[storeURL path] error:&error];
+		}
+		if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error]) {
+			NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+			abort();
+		}
+#else
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
+#endif
     }
 	
     return persistentStoreCoordinator;
