7b4778e135be4a9401775875cb28825f66f7db9b
Leif Harrison <lharrison@deloitte.com>
2014-07-20 21:40:20 -0700

    Lost dog

---
+++ Classes/Pathfinder/Containers/Profile/PFCharacterViewController.h
@@ -0,0 +1,22 @@
+//
+//  PFCharacterViewController.h
+//  CharMgr
+//
+//  Created by Leif Harrison on 9/6/12.
+//
+//
+
+#import "PFContainerViewController.h"
+
+@interface PFCharacterViewController : PFContainerViewController
+
+@property (nonatomic) IBOutlet UILabel *characterLabel;
+@property (nonatomic) IBOutlet UILabel *playerLabel;
+@property (nonatomic) IBOutlet UILabel *campaignLabel;
+
+@property (nonatomic) IBOutlet UILabel *alignmentLabel;
+@property (nonatomic) IBOutlet UILabel *genderLabel;
+@property (nonatomic) IBOutlet UILabel *raceLabel;
+@property (nonatomic) IBOutlet UILabel *sizeLabel;
+
+@end
